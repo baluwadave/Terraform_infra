b0ab3b3f40ace8f763b861f2c7adb780fd09b262
Balu <baluwadave@gmail.com>
2024-03-19 19:57:25 +0530

    --- Auto Git Commit ---

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,8 +1,45 @@
-module "ec2_instance" {
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.41.0"
+    }
+  }
+}
+
+
+
+provider "aws" {
+    region = "ap-northeast-3"
+    shared_config_files =["/root/.aws/config"]  
+    shared_credentials_files = ["/root/.aws/credentials"]
+    profile = "balu"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-staging-environmet-backend"
+    key = "Terraform/terraform.tfstate"
+    region ="ap-south-1"
+    dynamodb_table = "Terraform-server-backend"   
+  }
+}
+
+
+
+
+
+
+
+module "My_instances" {
     source = "./ec2"
-    count = 4
+    count =   var.instance_count
     ami =var.image_id
     instance_type = var.instance_type
     # vpc_security_group_ids = var.sg_id
-    key_name = var.key_name    
+    key_name = var.key_name
+    Name= var.project
+    environmet=var.environment
 }
