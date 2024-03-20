269fb7cd52590646927134af2b18d0372350ff9c
Balu <baluwadave@gmail.com>
2024-03-20 17:52:38 +0530

    --- Auto Git Commit ---

--- modules/output.tf
+++ modules/output.tf
@@ -10,6 +10,6 @@ output "instance_ids" {
 
 # Add more outputs as needed
 output "subnet_id1" {
-  value = module.terraform-vpc.public_subnet_id
+  value = module.vpc.public_subnet_id
   description="This is my public subnet id"
 }
