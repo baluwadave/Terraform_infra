36ef7f6b7b9e8ee6d7fce7ad0dcd00464150769b
Balu <baluwadave@gmail.com>
2024-03-20 17:45:26 +0530

    --- Auto Git Commit ---

--- modules/main.tf
+++ modules/main.tf
@@ -34,7 +34,7 @@ module "my_instances" {
   ami_id         = var.ami_id
   instance_type  = var.instance_type
   key_name       = var.key_name
-  subnet_id = module.vpc.public_subnet_id
+  subnet_id = module.terraform-vpc.public_subnet_id
 }
 
 # Other configurations can be added here
