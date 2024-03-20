0a88c2a2336907b5d8a9786b754f7e6d333cbe96
Balu <baluwadave@gmail.com>
2024-03-20 13:19:11 +0530

    --- Auto Git Commit ---

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "my_instances" {
   count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
+  key_name = var.key_name
 
   tags = {
     Name = "Instance-${count.index}"
--- modules/ec2_instance/variable.tf
+++ modules/ec2_instance/variable.tf
@@ -12,3 +12,7 @@ variable "instance_type" {
   description = "The type of EC2 instance to create"
   default     = "t2.micro"
 }
+variable "key_name" {
+    default = "pair"
+    description = "This is my key in that region"
+}
