6c82ea02e8d28345f6c06f165588f027d6037310
Balu <baluwadave@gmail.com>
2024-03-20 13:53:40 +0530

    --- Auto Git Commit ---

--- modules/output.tf
+++ modules/output.tf
@@ -1,12 +1,11 @@
 output "instance_ids" {
   description = "The IDs of the created EC2 instances"
-  value       = aws_instance.my_instances[*].id
+  value       = module.my_instances[*].id
 }
 
-output "public_ip_addresses" {
-  description = "The public IP addresses of the created EC2 instances"
-  value       = aws_instance.my_instances[*].public_ip
-}
+# output "public_ip_addresses" {
+#   description = "The public IP addresses of the created EC2 instances"
+#   value       = module.my_instances[*].public_ip
+# }
 
 # Add more outputs as needed
-    
