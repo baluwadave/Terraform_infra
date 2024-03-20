3502269fa65793907ce0dd77aa4b6ed40c848cb6
Balu <baluwadave@gmail.com>
2024-03-20 17:08:36 +0530

    --- Auto Git Commit ---

--- modules/main.tf
+++ modules/main.tf
@@ -10,7 +10,14 @@ terraform {
   }
 }
 
-
+terraform {
+  backend "s3" {
+    bucket = "terraform-staging-environmet-backend"
+    key = "Terraform/terraform.tfstate"
+    region ="ap-south-1"
+    dynamodb_table = "Terraform-server-backend"   
+  }
+}
 
 provider "aws" {
     region = "ap-northeast-3"
