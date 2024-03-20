174f020d0fe37b122ef070aba8eaab7b5fb622f5
Balu <baluwadave@gmail.com>
2024-03-20 16:33:47 +0530

    --- Auto Git Commit ---

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,8 +1,11 @@
 # Define EC2 instances
 resource "aws_instance" "my_instances" {
+  depends_on = [ vpc.vpc_id ]
   count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
+  subnet_id = var.subnet_id1
+#   vpc_security_group_ids = [  ]
   key_name = var.key_name
 
   tags = {
