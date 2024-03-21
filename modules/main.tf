04027b616d4493fe98ff53356c852b32afd202db
Balu <baluwadave@gmail.com>
2024-03-21 18:11:27 +0530

    --- Auto Git Commit ---

--- modules/main.tf
+++ modules/main.tf
@@ -73,7 +73,7 @@ module "alb" {
   # security_group_ids = module.Terraform_sg.Terraform_sg.id
   protocol = var.protocol
   target_group_name = var.target_group_name
-  instance =module.my_instances1.instance_ids
+
 
  # Replace with your security group IDs
 }
