d6a3230856795165680f1a0256a87b040d1bea3b
Balu <baluwadave@gmail.com>
2024-03-21 18:09:47 +0530

    --- Auto Git Commit ---

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -4,6 +4,7 @@ resource "aws_lb" "alb" {
   load_balancer_type = "application"
   # security_groups  =   var.security_group_ids
   subnets            = var.subnet_ids
+  instances =var.instance_id
   # vpc_id =var.vpc_id
   enable_deletion_protection = false
 }
