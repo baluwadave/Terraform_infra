817baadca083d4e562e8a76e532b7b1c3e0e42a4
Balu <baluwadave@gmail.com>
2024-03-21 19:03:50 +0530

    --- Auto Git Commit ---

--- modules/ec2_instance/output.tf
+++ modules/ec2_instance/output.tf
@@ -1,5 +1,5 @@
 output "instance_ids" {
   description = "The IDs of the created EC2 instances"
-  value       = aws_instance.my_instances.id
+  value       = aws_instance.my_instances[count.index]
 }
     
