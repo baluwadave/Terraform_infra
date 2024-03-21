6da486a12387a6d130b6d84bfa53bdeacfbbd5c4
Balu <baluwadave@gmail.com>
2024-03-21 15:07:10 +0530

    --- Auto Git Commit ---

---
+++ modules/loadbalancer/main.tf
@@ -0,0 +1,35 @@
+resource "aws_lb" "alb" {
+  name               = var.name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_group_ids
+  subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = var.protocol
+
+  default_action {
+    type             = "fixed-response"
+    status_code      = "200"
+    content_type     = "text/plain"
+    message_body     = "Hello from ALB!"
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = var.target_group_name
+  port        = 80
+  protocol    = var.protocol
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_lb_target_group_attachment" "target_attachment" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_lb_target_group.target_group.id  # Replace with your target instance ID
+}
---
+++ modules/loadbalancer/variable.tf
@@ -0,0 +1,28 @@
+variable "name" {
+  description = "Name of the ALB"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the ALB"
+  type        = list(string)
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs for the ALB"
+  type        = list(string)
+}
+
+variable "target_group_name" {
+  description = "Name of the target group"
+  type        = string
+}
+
+variable "protocol" {
+    type = string
+}
