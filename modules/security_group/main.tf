8407a630032d4464e3edfb1029c8e4b28512b706
Balu <baluwadave@gmail.com>
2024-03-21 13:26:08 +0530

    --- Auto Git Commit ---

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -4,12 +4,16 @@ resource "aws_security_group" "Terraform-sg" {
 
   vpc_id = var.vpc # Replace with your VPC ID
 
+  tags = {
+    Name=Terraform-sg
+  }
+
   # Ingress rules
   dynamic "ingress" {
     for_each = var.ports
     content {
-      from_port   = ingress.value
-      to_port     = ingress.value
+      from_port   = 20
+      to_port     = 65000
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]  # Allowing traffic from any IPv4 address, you may need to adjust this
     }
@@ -19,17 +23,17 @@ resource "aws_security_group" "Terraform-sg" {
   dynamic "egress" {
     for_each = var.ports
         content {
-      from_port   = egress.value
-      to_port     = egress.value
+      from_port   = 20
+      to_port     = 65000
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]  # Allowing traffic from any IPv4 address, you may need to adjust this
     }
     
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 }
