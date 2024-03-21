b0e6c4c5105e2b3285f073132b98fc506624a051
Balu <baluwadave@gmail.com>
2024-03-21 15:50:31 +0530

    --- Auto Git Commit ---

--- modules/loadbalancer/variable.tf
+++ modules/loadbalancer/variable.tf
@@ -3,10 +3,7 @@ variable "name" {
   type        = string
 }
 
-variable "vpc_id" {
-  description = "ID of the VPC"
-  type        = string
-}
+variable "vpc_id" {}
 
 variable "subnet_ids" {
   description = "List of subnet IDs for the ALB"
