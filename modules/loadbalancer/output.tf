3f51138669ebd352939abb04b7c6f1f06c3acd00
Balu <baluwadave@gmail.com>
2024-03-22 14:58:15 +0530

    --- Auto Git Commit ---

--- modules/loadbalancer/output.tf
+++ modules/loadbalancer/output.tf
@@ -1,3 +1,7 @@
-output "target_group_arn" {
-    value = aws_lb_target_group_attachment.target_attachment.target_group_arn
+
+
+output "target_group_arns" {
+  value = {
+    for key, attachment in aws_lb_target_group_attachment.target_attachment : key => attachment.target_group_arn
+  }
 }
