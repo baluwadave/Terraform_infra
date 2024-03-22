298b76d263eb49b5e105845a7f8cd6f0bce81194
Balu <baluwadave@gmail.com>
2024-03-22 13:00:47 +0530

    --- Auto Git Commit ---

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -26,7 +26,7 @@ resource "aws_lb_target_group" "target_group" {
   name        = var.target_group_name
   port        = 80
   protocol    = var.protocol
-  vpc_id      = var.vpc_id  
+  # vpc_id      = var.vpc_id  
   target_type = "instance"
     health_check {
     enabled             = true
