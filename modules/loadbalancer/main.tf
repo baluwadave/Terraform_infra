ea5196a929edb52a308e413ddde6c44fdd4ffbb1
Balu <baluwadave@gmail.com>
2024-03-26 14:15:18 +0530

    --- Auto Git Commit ---

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -63,8 +63,7 @@ resource "aws_lb_target_group" "target_group" {
     path                = "/"
     port                = 80
     protocol            = "HTTP"
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
+    unhealthy_threshold = 2
     matcher             = "200-499"
   }
 
