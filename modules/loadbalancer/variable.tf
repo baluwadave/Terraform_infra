dc552efe68b64a8e74b6eab0dbd5d88fc4b741ea
Balu <baluwadave@gmail.com>
2024-03-26 13:09:52 +0530

    --- Auto Git Commit ---

--- modules/loadbalancer/variable.tf
+++ modules/loadbalancer/variable.tf
@@ -31,25 +31,25 @@ variable "protocol" {
 
 
 
-variable "instance_count" {
-  description = "The number of EC2 instances to create"
-  type        = number
-  default     = 3
-}
+# variable "instance_count" {
+#   description = "The number of EC2 instances to create"
+#   type        = number
+#   default     = 3
+# }
 
-variable "ami_id" {
-  description = "The ID of the AMI to use for EC2 instances"
-  default = "ami-021a9d8a7dda97aa5"
-}
+# variable "ami_id" {
+#   description = "The ID of the AMI to use for EC2 instances"
+#   default = "ami-021a9d8a7dda97aa5"
+# }
 
-variable "instance_type" {
-  description = "The type of EC2 instance to create"
-  default     = "t3.micro"
-}
-variable "key_name" {
-    default = "pair"
-    description = "This is my key in that region"
-}
+# variable "instance_type" {
+#   description = "The type of EC2 instance to create"
+#   default     = "t3.micro"
+# }
+# variable "key_name" {
+#     default = "pair"
+#     description = "This is my key in that region"
+# }
 
 
 
@@ -58,6 +58,6 @@ variable "subnet_id" {
   description = "This is subnet created while created vpc"
 }
 
-variable "Terraform_sg" {
-  type = string
-}
+# variable "Terraform_sg" {
+#   type = string
+# }
