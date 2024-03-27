9f4f3afacc48d32cceeb083d564eb4a395454e59
Balu <baluwadave@gmail.com>
2024-03-27 15:55:21 +0530

    --- Auto Git Commit ---

--- modules/output.tf
+++ modules/output.tf
@@ -3,12 +3,6 @@ output "instance_ids" {
   value       = module.my_instances[*].instance_ids
 }
 
-# output "public_ip_addresses" {
-#   description = "The public IP addresses of the created EC2 instances"
-#   value       = module.my_instances[*].public_ip
-# }
-
-# Add more outputs as needed
 output "subnet_id1" {
   value = module.vpc.public_subnet_1a
   description="This is my public subnet id"
